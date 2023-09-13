dee1d93c7dd00f9b8f78fd3d652d88c4f40c66f2
Gabriel <tanase.gabriel671@gmail.com>
2023-09-13 12:20:05 +0300

    add JS show picture from list function

---
+++ code.js
@@ -0,0 +1,47 @@
+function showSanta() {
+  
+    document.getElementById('imageSnt')
+        .style.display = "block";
+    document.getElementById('btnImgSnt')
+        .style.display = "none";
+}
+
+function redoShowSanta()
+{
+    document.getElementById('btnImgSnt')
+        .style.display = "block";
+    document.getElementById('imageSnt')
+        .style.display = "none";
+}
+
+function showDog() {
+
+    document.getElementById('imageDog')
+        .style.display = "block";
+    document.getElementById('btnImgDog')
+        .style.display = "none";
+}
+
+function redoShowDog()
+{
+    document.getElementById('btnImgDog')
+        .style.display = "block";
+    document.getElementById('imageDog')
+        .style.display = "none";
+}
+
+function showClean() {
+  
+    document.getElementById('imageClean')
+        .style.display = "block";
+    document.getElementById('btnImgClean')
+        .style.display = "none";
+}
+
+function redoShowClean()
+{
+    document.getElementById('btnImgClean')
+        .style.display = "block";
+    document.getElementById('imageClean')
+        .style.display = "none";
+}
